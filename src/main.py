862bc5d9fc196de140fbb293903d0779d4a6cd14
dmilanop <dmilanop28@gmail.com>
2022-02-07 17:12:41 +0000

    Entrega de proyecto

--- src/main.py
+++ src/main.py
@@ -9,12 +9,15 @@ from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
 from models import db, User
+from flask_jwt_extended import JWTManager, create_access_token, get_jwt_identity, jwt_required
 #from models import Person
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_CONNECTION_STRING')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['JWT_SECRET_KEY'] = os.environ.get('FLASK_APP_KEY')
+jwt = JWTManager(app)
 MIGRATE = Migrate(app, db)
 db.init_app(app)
 CORS(app)
@@ -38,6 +41,34 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+
+@app.route('/signup', methods=['POST'])
+def handle_sign_up():
+    body = request.json
+    new_user = User.register(body)
+
+    if new_user is not None:
+        try:
+            token = create_access_token(identity = new_user.id)
+            return jsonify({"token": token, "user_id": new_user.id, "email": new_user.email}), 201
+        except Exception as error:
+            return jsonify({"message": "Oops, Check if the fields are filled correctly"}), 500
+    else:
+        jsonify({"message": "Please try again"}), 500
+
+
+@app.route('/login', methods=['POST'])
+def handle_log_in():
+    email = request.json.get("email", None)
+    password = request.json.get("password", None)
+    user = User.query.filter_by(email = email, password = password).one_or_none()
+
+    if user is not None:
+        token = create_access_token(identity = user.id)
+        return jsonify({"token": token, "user_id": user.id, "email": user.email}), 200
+    else:
+        return jsonify({"message": "Put your credentials right"}), 401
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -6,14 +6,28 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    name = db.Column(db.String(50), unique=False, nullable=False)
+    last_name = db.Column(db.String(80), unique=False, nullable=False)
+    gender = db.Column(db.String(50), unique=False, nullable=False)
 
-    def __repr__(self):
-        return '<User %r>' % self.username
+    @classmethod
+    def register(cls, new_user):
+        new_user = cls(**new_user)
+        
+        db.session.add(new_user)
+        try:
+            db.session.commit()
+            return new_user
+        except Exception as error:
+            db.session.rollback()
+            return None
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
+            "name": self.name,
+            "last_name": self.last_name,
+            "gender": self.gender
             # do not serialize the password, its a security breach
         }
